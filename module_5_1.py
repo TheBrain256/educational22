c4aa9652f328d625339c4d66fb0dc3e05eedcd3a
TheBrain256 <Shimoro2576@gmail.com>
2024-11-30 20:50:01 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -4,7 +4,7 @@ class House:
         self.number_of_floors = number_of_floors
 
     def go_to(self, new_floor):
-        for i in range(1, new_floor +1):
+        for i in range(0, new_floor +1):
             if 1 <= new_floor <= self.number_of_floors:
                 print(i)
             else:
@@ -17,4 +17,4 @@ class House:
 h1 = House('ЖК Лучшая жизнь', 25)
 h2 = House('Хрущёвка', 5)
 h1.go_to(4)
-h2.go_to(7)
+h2.go_to(0)
